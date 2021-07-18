c3a381d3e246f0454f3f2dd070aa17e875e4af7e
MainaGeorge <nuriszka@gmail.com>
2021-07-18 14:06:42 +0200

    defined the entities of the application

---
+++ EFCoreWebApp.Core/Entities/LookUpType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EFCoreWebApp.Core.Entities
+{
+    public enum LookUpType
+    {
+        State=0,
+        Country=1
+    }
+
+    public class LookUp
+    {
+        [Key]
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public LookUpType LookUpType { get; set; }
+    }
+}
---
+++ EFCoreWebApp.Core/Entities/Person.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreWebApp.Core.Entities
+{
+    [Table("persons", Schema = "dbo")]
+    public class Person
+    {
+        [Column("Person_Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [Column(TypeName = "nvarchar(255)")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [Column(TypeName = "nvarchar(255)")]
+
+        public string LastName { get; set; }
+
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
+        public List<Address> Addresses { get; set; } = new List<Address>();
+
+    }
+}
