4ab6e3dde48cde6773af9f7bafc70bd3f0871e52
MainaGeorge <nuriszka@gmail.com>
2021-07-18 15:03:25 +0200

    seeded test data for all entities

---
+++ EFCore5WebApp.DAL/Migrations/20210718130106_SeedingPersons.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore5WebApp.DAL.Migrations
+{
+    public partial class SeedingPersons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "persons",
+                columns: new[] { "Id", "EmailAddress", "FirstName", "LastName" },
+                values: new object[] { 1, "john@smith.com", "John", "Smith" });
+
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "persons",
+                columns: new[] { "Id", "EmailAddress", "FirstName", "LastName" },
+                values: new object[] { 2, "john@smith.com", "Susan", "Jones" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "persons",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "persons",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
