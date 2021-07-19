20c59bc193a8403af65873ab9e9436ac9bc0721b
MainaGeorge <nuriszka@gmail.com>
2021-07-19 14:59:51 +0200

    added tests with stored procedures

---
+++ EFCore5WebApp.DAL.Tests/StoredProcedureTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Linq;
+using EFCoreWebApp.Core.Entities;
+
+namespace EFCore5WebApp.DAL.Tests
+{
+    [TestFixture]
+    public class StoredProcedureTests
+    {
+        private AppDbContext _context;
+        [SetUp]
+        public void SetUp()
+        {
+            _context = new AppDbContext(
+                new DbContextOptionsBuilder<AppDbContext>()
+                    .UseSqlServer(
+                        "Server=(localdb)\\mssqllocaldb;Database=EfCoreLearningWebApp;Trusted_Connection=True;MultipleActiveResultSets=true")
+                    .Options);
+        }
+
+        [Test]
+        public void GetPersonsByStateInterpolated()
+        {
+            const int expectedNumberOfPeopleCA = 1;
+            const string california = "CA";
+
+            var californiaResidents = _context.Persons
+                .FromSqlInterpolated($"GetPersonsByState {california}").ToList();
+            
+            Assert.AreEqual(expectedNumberOfPeopleCA, californiaResidents.Count);
+        }
+
+        [Test]
+        public void GetPersonsByStateRaw()
+        {
+            const int expectedNumberOfPeopleIL = 2;
+            const string illinois = "IL";
+
+            var illinoisResidents = _context.Persons
+                .FromSqlRaw($"GetPersonsByState @p0", new object[]{illinois}).ToList();
+
+            Assert.AreEqual(expectedNumberOfPeopleIL, illinoisResidents.Count);
+        }
+
+        [Test]
+        public void AddLookUpItemInterpolated()
+        {
+            const string code = "CAN";
+            const string description = "Canada";
+            const LookUpType lookUpType = LookUpType.Country;
+
+            _context.Database.ExecuteSqlInterpolated($"AddLookUpItem {code}, {description}, {lookUpType}");
+
+            var addedLookUpType = _context.LookUps.Single(x => x.Code == "CAN");
+
+            Assert.IsNotNull(addedLookUpType);
+
+            _context.LookUps.Remove(addedLookUpType);
+            _context.SaveChanges();
+        }
+
+        [Test]
+        public void AddLookUpItemRawSql()
+        {
+            const string code = "MEX";
+            const string description = "Mexico";
+            const LookUpType lookUpType = LookUpType.Country;
+
+            _context.Database.ExecuteSqlRaw("AddLookUpItem @p0, @p1, @p2",
+                new object[] {code, description, lookUpType}
+            );
+
+            var addedLookUpType = _context.LookUps.Single(x => x.Code == "MEX");
+
+            Assert.IsNotNull(addedLookUpType);
+
+            _context.Remove(addedLookUpType);
+            _context.SaveChanges();
+        }
+
+    }
+}
---
+++ EFCore5WebApp.DAL/Migrations/20210719120315_AddingStoredProcedureToTheDatabase.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore5WebApp.DAL.Migrations
+{
+    public partial class AddingStoredProcedureToTheDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "PersonPerson",
+                newName: "PersonPerson",
+                newSchema: "dbo");
+
+            var procedure1 = @"
+                IF OBJECT_ID('GetPersonsByState', 'P') IS NOT NULL
+                DROP PROC GetPersonsByState
+                GO
+                CREATE PROCEDURE [dbo].[GetPersonsByState]
+                @State varchar(255)
+                AS
+                BEGIN
+                SELECT p.*
+                FROM Persons p
+                INNER JOIN Addresses a on p.Id = a.PersonId
+                WHERE a.State = @State
+                END";
+
+            var procedure2 = @"
+                IF OBJECT_ID('AddLookUpItem', 'P') IS NOT NULL
+                DROP PROC AddLookUpItem
+                GO
+                CREATE PROCEDURE [dbo].[AddLookUpItem]
+                @Code varchar(255),
+                @Description varchar(255),
+                @LookUpTypeId int
+                AS
+                BEGIN
+                INSERT INTO LookUps (Code, Description, LookUpType) VALUES (@Code,
+                @Description, @LookUpTypeId)
+                END";
+
+            migrationBuilder.Sql(procedure2);
+            migrationBuilder.Sql(procedure1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "PersonPerson",
+                schema: "dbo",
+                newName: "PersonPerson");
+
+            migrationBuilder.Sql(@"DROP PROC GetPersonsByState");
+            migrationBuilder.Sql(@"DROP PROC AddLookUpItem");
+        }
+    }
+}
